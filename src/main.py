0f0fabec811dd0960cf9e35a6c51d16c8c1e17f8
Natanco1 <nata.oliveira2004@gmail.com>
2025-02-25 15:55:55 -0300

    feat: add expiration date

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import os
 import time
 import py7zr
+from datetime import datetime
 
-def monitor_and_chunk(input_path, output_path, treshold, chunk):
+def monitor_and_chunk(input_path, output_path, threshold, chunk, expiration_seconds):
     if not os.path.exists(input_path):
         os.makedirs(input_path)
         print(f"Created input directory: {input_path}")
@@ -11,16 +12,23 @@ def monitor_and_chunk(input_path, output_path, treshold, chunk):
         os.makedirs(output_path)
         print(f"Created output directory: {output_path}")
     
-    chunk = min(chunk, treshold)
+    chunk = min(chunk, threshold)
     
     while True:
         try:
             files = sorted(os.listdir(input_path)) 
             files = [f for f in files if os.path.isfile(os.path.join(input_path, f))]
             
-            if len(files) >= treshold:
-                chunk_files = files[:chunk]
-                
+            expired_files = []
+            current_time = datetime.now()
+            for file in files:
+                file_path = os.path.join(input_path, file)
+                file_mtime = datetime.fromtimestamp(os.path.getmtime(file_path))
+                if (current_time - file_mtime).total_seconds() > expiration_seconds:
+                    expired_files.append(file)
+            
+            if len(expired_files) >= threshold:
+                chunk_files = expired_files[:chunk]
                 first_file = chunk_files[0]
                 last_file = chunk_files[-1]
                 archive_name = f"{first_file.split('.')[0]}_{last_file.split('.')[0]}.7z"
@@ -43,8 +51,9 @@ def monitor_and_chunk(input_path, output_path, treshold, chunk):
         time.sleep(1)
 
 input_path = "./data"
-treshold = 10
-chunk = 10
+threshold = 100
+chunk = 100
 output_path = "./compressed"
+expiration_seconds = 60 
 
-monitor_and_chunk(input_path, output_path, treshold, chunk)
+monitor_and_chunk(input_path, output_path, threshold, chunk, expiration_seconds)
